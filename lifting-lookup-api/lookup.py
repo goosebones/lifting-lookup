dbdd0f24ac20d299664511944447c49a7d56b544
Gunther Kroth <gdkroth@gmail.com>
2023-11-06 22:29:32 -0500

    web scraping is now threaded

--- lifting-lookup-api/lookup.py
+++ lifting-lookup-api/lookup.py
@@ -2,114 +2,101 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from tqdm import tqdm
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-liftingcast_home = "https://liftingcast.com/"
+from lookup import LiftingCast
 
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import queue
 
-class LiftingCast:
-    def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument("headless")
-        driver = webdriver.Chrome(
-            options=options, service=ChromeService(ChromeDriverManager().install())
-        )
-        driver.implicitly_wait(2)
-        driver.get(liftingcast_home)
-        self.driver = driver
-        self.meets = []
-        self.lifters = []
+import os
 
-    def fetch_meets(self):
-        tables = self.driver.find_elements(By.CLASS_NAME, "table")
-        upcoming_meets = None
-        recent_meets = None
-        for table in tables:
-            head = table.find_element(By.XPATH, ("./thead"))
-            if "Upcoming Meets" in head.text:
-                upcoming_meets = table
-            elif "Recent Meets" in head.text:
-                recent_meets = table
+import concurrent.futures
 
-        meets = []
-        for meet_table in [upcoming_meets, recent_meets]:
-            rows = meet_table.find_elements(By.XPATH, ("./tbody/tr"))
-            for row in rows:
-                [name, date] = row.find_elements(By.XPATH, ("./td"))
-                meet_id = (
-                    name.find_element(By.XPATH, ("./a"))
-                    .get_attribute("href")
-                    .split("/")[4]
-                )
-                meets.append({"name": name.text, "date": date.text, "meet_id": meet_id})
-        self.meets = meets
-
-    def fetch_lifters(self):
-        for meet in tqdm(self.meets):
-            meet["Lifters"] = []
-            meet_url = f"https://liftingcast.com/meets/{meet['meet_id']}/roster"
-            self.driver.get(meet_url)
-            lifters = self.driver.find_elements(By.TAG_NAME, "a")
-            for lifter in lifters:
-                lifter_id = lifter.get_attribute("href").split("/")[-1]
-                meet["Lifters"].append(
-                    {
-                        "name": lifter.text,
-                        "lifter_id": lifter_id,
-                        "meet": meet["name"],
-                        "meet_id": meet["meet_id"],
-                    }
-                )
-
-    def fetch_lifter_details(self):
-        for meet in tqdm(self.meets):
-            for lifter in meet["Lifters"]:
-                meet_lifter_url = f"https://liftingcast.com/meets/{meet['meet_id']}/lifter/{lifter['lifter_id']}/info"
-                self.driver.get(meet_lifter_url)
-                informations = self.driver.find_elements(By.CLASS_NAME, "info-row")
-                for info in informations:
-                    label = info.find_element(By.TAG_NAME, "label").text.replace(
-                        ":", ""
-                    )
-                    value = info.find_element(By.TAG_NAME, "div").text
-                    lifter[label] = value
-
-    def get_lifters(self):
-        return [meet["Lifters"] for meet in self.meets]
-
-
-from multiprocessing import Pool
-
-
-def fetch_lifters_from_meet(meet):
+def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
+    options.add_argument("--log-level=3")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(
         options=options, service=ChromeService(ChromeDriverManager().install())
     )
     driver.implicitly_wait(2)
+    return driver
+
+
+def fetch_lifters_from_meet(meet, driver_queue, all_lifters, lock):
+    print(f"fetching lifters for {meet['name']}")
+    try:
+        print(f"{meet['name']} attempting to get driver from queue")
+        with lock:
+            driver = driver_queue.get(timeout=5)
+        print(f"{meet['name']} successfuly got driver from queue")
+    except queue.Empty:
+        print(f"{meet['name']} could not get a driver from the queue")
     meet_url = f"https://liftingcast.com/meets/{meet['meet_id']}/roster"
-    driver.get(meet_url)
-    lifters = driver.find_elements(By.TAG_NAME, "a")
-    result = []
-    for lifter in lifters:
-        lifter_id = lifter.get_attribute("href").split("/")[-1]
-        result.append(
-            {
-                "name": lifter.text,
-                "lifter_id": lifter_id,
-                "meet": meet["name"],
-                "meet_id": meet["meet_id"],
-            }
-        )
-    print(f"fetched {len(result)} lifters for {meet["name"]}")
-    return result
+    try:
+        driver.get(meet_url)
+        print(f"waiting for {meet['name']} to load")
+        wait = WebDriverWait(driver, timeout=5)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'meet-info-row')))
+        print(f"{meet['name']} finished loading, getting names now")
+        lifters = driver.find_elements(By.TAG_NAME, "a")
+        result = []
+        for lifter in lifters:
+            lifter_id = lifter.get_attribute("href").split("/")[-1]
+            result.append(
+                {
+                    "name": lifter.text,
+                    "lifter_id": lifter_id,
+                    "meet": meet["name"],
+                    "meet_id": meet["meet_id"],
+                }
+            )
+        print(f"fetched {len(result)} lifters for {meet["name"]}")
+        with lock:
+            all_lifters.extend(result)
+            driver_queue.put(driver)
+    except Exception as e:
+        print(e)
+    
+liftingcast = LiftingCast()
+liftingcast.fetch_meets()
+
+number_of_drivers = 12
+driver_queue = queue.Queue(number_of_drivers)
+for d in range(number_of_drivers):
+    driver_queue.put(get_driver())
+all_lifters = []
+lock = threading.Lock()
+
+with concurrent.futures.ThreadPoolExecutor(number_of_drivers) as executor:
+    for meet in liftingcast.meets:
+        executor.submit(fetch_lifters_from_meet, meet, driver_queue, all_lifters, lock)
+
+threads = []
+with ThreadPoolExecutor(max_workers=number_of_drivers-1) as executor:
+    for meet in liftingcast.meets:
+        thread = executor.submit(fetch_lifters_from_meet, meet, all_lifters, lock)
+
+while not driver_queue.empty():
+    driver = driver_queue.get()
+    driver.quit()
+
+if os.path.exists('lifters.txt'):
+    os.remove('lifters.txt')
+
+errored_lifters = 0
+with open('lifters.txt', 'w+') as f:
+    for lifter in all_lifters:
+        try:
+            f.write(f"{lifter}\n")
+        except:
+            errored_lifters += 1
+            print('unable to write lifter')
+    
+print(f'number of errored lifteres: {errored_lifters}')
 
 
-if __name__ == "__main__":
-    liftingcast = LiftingCast()
-    liftingcast.fetch_meets()
-    pool = Pool(10)
-    lifters_for_each_meet = pool.map(fetch_lifters_from_meet, liftingcast.meets[:10])
-    all_lifters = [lifter for meet in lifters_for_each_meet for lifter in meet]
-    print(len(all_lifters))
