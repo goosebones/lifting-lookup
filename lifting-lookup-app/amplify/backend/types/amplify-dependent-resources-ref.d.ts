793205f42db08992c9f2680cd6b75589998a7a89
Gunther Kroth <gdkroth@gmail.com>
2024-02-22 17:08:58 -0500

    update amplify environment

--- lifting-lookup-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ lifting-lookup-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "liftinglookupappapi": {
+    "liftinglookupapp": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string"
     }
